bc6515afb258704b026e4537abea07e29e6e62d8
doctorpatitas <diegogonzal272111@gmail.com>
2026-06-02 14:44:31 -0500

    semana 3 casi terminada

--- gestion_datos.js
+++ gestion_datos.js
@@ -1,75 +1,33 @@
-//TAREA 1
-//Creamos el objeto 
-let producto = {
-    yuca: {
-        id: 1,
-        nombre: "yuca",
-        precio: 2000
-    },
-    piña: {
-        id: 2,
-        nombre: "piña",
-        precio: 1500
-    },
-    tomate: {
-        id: 3,
-        nombre: "tomate",
-        precio: 2500
-    },
-    ñame: {
-        id: 4,
-        nombre: "ñame",
-        precio: 3000
-    }
-}
+//TASK 2
+//Seleccionamos el input, boton y la lista desorganizada 
+//Seleccionamos primero el input
+const input = document.querySelector("#entrada");
+
+//Seleccionamos ahora el boton
+const miBoton = document.querySelector("#botonAgregar");
+
+//Ahora seleccionamos la lista desorganizada
+const listaDeNotas = document.querySelector("#listaNotas");
+
+//Y ahora los imprimimos para verificar que existen
+console.log(input);
+console.log(miBoton);
+console.log(listaDeNotas);
 
 
-//TAREA 2
-//Creamos la lista de números repetidos del 1 al 20
-const listaDeRepetidos = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17,18,18,19,19,20,20];
-
-//Se crea un nuevo Set sin números repetidos usando el set y luego se imprime
-const listaSinRepetidos = new Set(listaDeRepetidos);
-console.log(listaSinRepetidos);
-
-//Agregamos un nuevo número al Set y luego lo imprimimos
-listaSinRepetidos.add(21);
-console.log(listaSinRepetidos);
-
-//Verificamos sin un número en especifico existe dentro del Set con ".has()"
-console.log(listaSinRepetidos.has(20));
-
-//Eliminamos el nuevo número que agregamos anteriormente con ".delete()"
-listaSinRepetidos.delete(21);
-
-//Recorremos el nuevo Set con "for...of" y imprimimos los valores
-for (const numeros of listaDeRepetidos){
-    console.log(numeros);
-}
-
-
-//TAREA 3
-//Creamos un map
-const miNuevoMap = new Map([
-    ['marcasCarros','twingo'],
-    ['sistemasOperativos','windows'],
-    ['moda','gucci'],
-    ['paises','colombia']
-])
-
-//TAREA 4
-//Recorremos todos los datos y los imprimimos por consola
-//for...in
-for (item in producto) {
-    console.log(producto);
-}
-
-//for..of
-for (numero of listaSinRepetidos) {
-    console.log(listaSinRepetidos);
-}
-
-//forEach()
-miNuevoMap.forEach((clave, valor) => {
-    console.log(clave + ": " + valor);
+//TASK 3
+//Agregamos una función para que cuando escuche el evento del click, agregue el contenido del input a la lista, para luego mostrarlo en pantalla
+miBoton.addEventListener("click", () => {
+    const nota = input.value;
+    listaDeNotas.innerHTML += `
+    <li id="itemLista">
+    ${nota} <button onclick="eliminarNota(this)" id="botonEliminar">X</button>
+    </li>
+    `;
 })
+
+//Codigo del boton que elimina la nota
+function eliminarNota(item) {
+    const nota = item.closest("#itemLista");
+    nota.remove()
+}
